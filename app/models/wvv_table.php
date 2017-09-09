6f81c6ef5e70fc70f5b517244a463999102d4bd7
Renão <me@renao.de>
2017-09-09 22:25:46 +0200

    Update WVVTable model

--- app/models/wvv_table.php
+++ app/models/wvv_table.php
@@ -1,29 +1,16 @@
 <?php
 
 class WVVTable {
-    public $table_entries = [];
+    public $tableEntries = [];
 
-    public function __construct(XMLReader $xml_content) {
-        prepare_from_xml($xml_content);
-    }
-
-    private function prepare_from_xml($xml_content) {
-        $xml_content::moveToAttribute("tabellen");
-        update_table_from_xml($xml_content::readInnerXML());
-    }
-
-    private function update_table_from_xml($elements_xml) {
-        $this->table_entries = [];
-        $dom = new DOMDocument();
-        $dom->loadXML($elements_xml);
-
-        foreach ($dom->getElementByTagName("element") as $entry) {
-            array_push($this->table_entries, createEntry($entry));
+    public function __construct(SimpleXMLElement $tableXml) {
+        foreach ($tableXml->children() as $elementName => $elementNode) {
+            array_push($this->tableEntries, $this->createEntry($elementNode));
         }
     }
 
-    private function createEntry($entry_xml) {
-        return new TableEntry($entry_xml);
+    private function createEntry(SimpleXMLElement $tableElement) {
+        return new TableEntry($tableElement);
     }
 }
 
