20affd36b4081f8b3dc0b70307a80bfbd4acaa29
Renão <me@renao.de>
2017-09-10 00:35:47 +0200

    Add TablePresenter

---
+++ app/presenters/table_presenter.php
@@ -0,0 +1,19 @@
+<?php
+
+class TablePresenter {
+
+    public static function render(WVVTable $table) {
+        $htmlBody = file_get_contents(__DIR__ . "/../templates/table.html");
+
+        if (count($table->tableEntries > 0)) {
+            $entriesHtml = "";
+            foreach ($table->tableEntries as $entry) {
+                $entriesHtml .= TableEntryPresenter::render($entry);
+            }
+
+            return str_replace("{{%table_entries}}", $entriesHtml, $htmlBody);
+        }
+    }
+}
+
+?>
