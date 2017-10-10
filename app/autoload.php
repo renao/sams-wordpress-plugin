93fc8f38c69b51246f0f28ef23f9cd7d627ac0f7
Renão <me@renao.de>
2017-10-10 22:12:51 +0200

    Add workaround autoload.php

---
+++ app/autoload.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("models/table_entry.php");
+    require_once("models/wvv_table.php");
+    require_once("presenters/table_entry_presenter.php");
+    require_once("presenters/table_presenter.php");
+    require_once("remote/uris/table_uri.php");
+    require_once("remote/xml_fetcher.php");
+    require_once("wvv_table_renderer.php");
+?>
