d9c4d322c763bd32e7022143669f95970b118b4e
Renão <me@renao.de>
2017-10-13 22:41:34 +0200

    Update manual autoload

--- app/autoload.php
+++ app/autoload.php
@@ -6,4 +6,11 @@
     require_once("remote/uris/table_uri.php");
     require_once("remote/xml_fetcher.php");
     require_once("wvv_table_renderer.php");
+
+    require_once("models/fixtures_entry.php");
+    require_once("models/wvv_fixtures.php");
+    require_once("presenters/fixtures_entry_presenter.php");
+    require_once("presenters/fixtures_presenter.php");
+    require_once("remote/uris/fixtures_uri.php");
+    require_once("wvv_fixtures_renderer.php");
 ?>
