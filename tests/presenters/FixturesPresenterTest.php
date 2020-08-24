814fde6754d31c0c2c5aa99ce54c87e64b0c0565
René Wernicke <r.wernicke@prosoz.de>
2020-08-24 22:48:11 +0200

    Fix failing test

--- tests/presenters/FixturesPresenterTest.php
+++ tests/presenters/FixturesPresenterTest.php
@@ -12,7 +12,7 @@ final class FixturesPresenterTest extends TestCase {
         $rendered = FixturesPresenter::render($fixtures, "TuS Herten");
         $rawTemplate = file_get_contents(__DIR__ . "/../../app/templates/fixtures.html");
 
-        $this->assertNull($rendered);
+        $this->assertNotNull($rendered);
         $this->assertNotEquals($rawTemplate, $rendered);
     }
 }
