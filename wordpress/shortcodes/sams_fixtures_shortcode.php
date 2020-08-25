1f623c8a8d2f8572f60e66d5c4fb52691fc37064
René Wernicke <r.wernicke@prosoz.de>
2020-08-26 00:44:50 +0200

    Lowercase shortcode attributes

--- wordpress/shortcodes/sams_fixtures_shortcode.php
+++ wordpress/shortcodes/sams_fixtures_shortcode.php
@@ -5,11 +5,10 @@ use SAMSPlugin\SAMSFixturesRenderer;
 
 function fetch_and_render_sams_fixtures( $atts ) {
     $a = shortcode_atts( array(
-        'apiKey' => '',
-        'matchSeriesId' => '21416892',
-        'teamId' => '21416933'
+        'apikey' => 'asd',
+        'matchseriesid' => '21416892',
+        'teamid' => '21416933'
     ), $atts );
-
-    return SAMSFixturesRenderer::fetchAndRender($a['apiKey'], $a['matchSeriesId'], $a['teamId']);
+    return SAMSFixturesRenderer::fetchAndRender($a['apikey'], $a['matchseriesid'], $a['teamid']);
 }
 ?>
