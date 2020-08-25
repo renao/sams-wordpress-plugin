c3d476feb03e9e04d775b6644581174ed2a48912
René Wernicke <r.wernicke@prosoz.de>
2020-08-25 23:21:51 +0200

    Add API Key to presenter render calls

--- wordpress/shortcodes/sams_fixtures_shortcode.php
+++ wordpress/shortcodes/sams_fixtures_shortcode.php
@@ -5,10 +5,11 @@ use SAMSPlugin\SAMSFixturesRenderer;
 
 function fetch_and_render_sams_fixtures( $atts ) {
     $a = shortcode_atts( array(
+        'apiKey' => '',
         'matchSeriesId' => '21416892',
         'teamId' => '21416933'
     ), $atts );
 
-    return SAMSFixturesRenderer::fetchAndRender($a['matchSeriesId'], $a['teamId']);
+    return SAMSFixturesRenderer::fetchAndRender($a['apiKey'], $a['matchSeriesId'], $a['teamId']);
 }
 ?>
--- wordpress/shortcodes/sams_table_shortcode.php
+++ wordpress/shortcodes/sams_table_shortcode.php
@@ -5,9 +5,10 @@ use SAMSPlugin\SAMSTableRenderer;
 
 function fetch_and_render_sams_table( $atts ) {
     $a = shortcode_atts( array(
+        'apiKey' => '',
         'matchSeriesId' => '21416892'
     ), $atts );
 
-    return SAMSTableRenderer::fetchAndRender($a['matchSeriesId']);
+    return SAMSTableRenderer::fetchAndRender($a['apiKey'], $a['matchSeriesId']);
 }
 ?>
