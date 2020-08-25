f8de1fe16a1a6f33de196247fb56d8ed5bfb86a2
René Wernicke <r.wernicke@prosoz.de>
2020-08-26 00:26:30 +0200

    Make shortcode attributes work by using lowercase

--- wordpress/shortcodes/sams_table_shortcode.php
+++ wordpress/shortcodes/sams_table_shortcode.php
@@ -5,10 +5,10 @@ use SAMSPlugin\SAMSTableRenderer;
 
 function fetch_and_render_sams_table( $atts ) {
     $a = shortcode_atts( array(
-        'apiKey' => '',
-        'matchSeriesId' => '21416892'
+        'apikey' => 'asd',
+        'matchseriesid' => '21416892'
     ), $atts );
 
-    return SAMSTableRenderer::fetchAndRender($a['apiKey'], $a['matchSeriesId']);
+    return SAMSTableRenderer::fetchAndRender($a['apikey'], $a['matchseriesid']);
 }
 ?>
