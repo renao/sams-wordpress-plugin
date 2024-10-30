c0874c90238120d510a61b169afc8d26b475a902
René Siemer <me@renao.de>
2024-10-30 18:33:52 +0100

    Implement fixtures block

--- src/blocks/sams-fixtures/render.php
+++ src/blocks/sams-fixtures/render.php
@@ -2,7 +2,55 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+
+use SAMSPlugin\FixturesFetcher;
+
+if (isset($attributes)
+	&& isset($attributes['apiKey'])
+	&& isset($attributes['associationUrl'])
+	&& isset($attributes['matchSeriesId'])
+	&& isset($attributes['teamId'])) {
+
+	$fetcher = new FixturesFetcher();
+	$fixtures = $fetcher->fetch(
+		$attributes['associationUrl'],
+		$attributes['apiKey'],
+		$attributes['matchSeriesId'],
+		$attributes['teamId']);
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'SAMS Fixtures – hello from a dynamic block!', 'sams-plugin' ); ?>
-</p>
+
+<div <?php echo get_block_wrapper_attributes(); ?>>
+<table class="sams-fixtures">
+    <th class="date"><?php esc_html_e('Starts', 'sams-plugin'); ?></th>
+    <th class="fixtureName"><?php esc_html_e('Fixture', 'sams-plugin'); ?></th>
+    <th class="venue"><?php esc_html_e('Venue', 'sams-plugin'); ?></th>
+    
+	
+	<?php foreach ($fixtures->fixturesEntries as $fixture) { ?>
+
+	<tr>
+    <td class="date">
+		<?php echo $fixture->date; ?>
+        <br>
+		<?php echo $fixture->startTime; ?> Uhr
+    </td>
+    <td class="fixture">
+		<span class="fixture-name"> <?php echo $fixture->teamHome . " - " . $fixture->teamAway ?></span>
+		<span class="fixture-result"><?php if ($fixture->hasResult()) { echo " (" . $fixture->score . ")"; }; ?></span>
+	</td>
+    <td class="venue"><?php echo $fixture->venue; ?></td>
+</tr>
+
+<?php };?>
+</table>
+
+<?php
+} else {
+	// Display error message on invalid configuration
+	esc_html_e('Error in SAMS Fixtures: Configuration missing or incomplete', 'sams-plugin');
+}
+	?>
+
+</div>
+
--- src/php/remote/uris/FixturesURI.php
+++ src/php/remote/uris/FixturesURI.php
@@ -4,19 +4,21 @@ namespace SAMSPlugin\Remote\URIs;
 
 class FixturesURI {
 
+    private $baseUrl;
     private $apiKey;
     private $matchSeriesId;
     private $teamId;
 
-    public function __construct($apiKey, $matchSeriesId, $teamId) {
-        $this->validateArguments($apiKey, $matchSeriesId, $teamId);
+    public function __construct($baseUrl, $apiKey, $matchSeriesId, $teamId) {
+        $this->validateArguments($baseUrl, $apiKey, $matchSeriesId, $teamId);
+        $this->baseUrl = $baseUrl;
         $this->apiKey = $apiKey;
         $this->matchSeriesId = $matchSeriesId;
         $this->teamId = $teamId;
     }
 
     public function toString() {
-        return "https://dvv.sams-server.de/xml/matches.xhtml?apiKey=" 
+        return rtrim($this->baseUrl, '/') . "/xml/matches.xhtml?apiKey=" 
         . $this->apiKey 
         ."&matchSeriesId=" 
         . $this->matchSeriesId 
@@ -24,9 +26,9 @@ class FixturesURI {
         . $this->teamId;
     }
 
-    private function validateArguments($apiKey, $matchSeriesId, $teamId) {
-        if (($apiKey == null) || ($matchSeriesId == null) || ($teamId == null)) {
-            throw new \InvalidArgumentException("API-Key, MatchSeriesId and TeamID cannot be NULL");
+    private function validateArguments($baseUrl, $apiKey, $matchSeriesId, $teamId) {
+        if (($baseUrl == null) || ($apiKey == null) || ($matchSeriesId == null) || ($teamId == null)) {
+            throw new \InvalidArgumentException("Base-URL, API-Key, MatchSeriesId and TeamID cannot be NULL");
         }
     }
 }
